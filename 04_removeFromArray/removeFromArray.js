be2f8a510b72527a5f1428b8dcdae96058f88ae7
Ama Nkansah <amankansahstate@gmail.com>
2025-01-29 08:51:38 +0000

    passed my 4th jest testing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...anyargs) {
+  const arrayStored = array.filter(item => !anyargs.includes(item));
+    return arrayStored ;
 };
 
 // Do not edit below this line
